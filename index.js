af24ce119771de8e5a484b1d919718d21be61a48
cep104 <candicelizabethpeters@gmail.com>
2020-11-18 15:37:32 -0600

    commit tree ed04f0d6775abbdeded8554926e4add090bec63a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+   
+ 
+
+function submitData( name, email ){
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+      };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(object => document.body.innerHTML = object.id)
+    .catch(error => document.body.innerHTML = error.message
+      );
+  }
+   
+  
+  
